7de8f2067675abae82f478a9d4c826d733138bfd
Starukhin Denis <90477034+Flockandglock@users.noreply.github.com>
2022-09-01 16:13:49 +0400

    добавил инфу о работниках

--- src/components/app-info/app-info.js
+++ src/components/app-info/app-info.js
@@ -1,11 +1,21 @@
+import { useContext } from "react";
+import { EmplsContext } from "../../context";
+
 import "./app-info.css";
 
 const AppInfo = () => {
+
+    const {empls, setEmpls, loading} = useContext(EmplsContext);
+
+    const allEmpls = empls.length;
+
+    const willIncrease = empls.filter(empl => empl.increase).length;
+
     return (
         <div className="app-info">
             <h1>Учет сотрудников в компании N</h1>
-            <h2>Общее число сотрудников:</h2>
-            <h2>Премию получат:</h2>
+            <h2>Общее число сотрудников: {allEmpls}</h2>
+            <h2>Премию получат: {willIncrease}</h2>
         </div>
     )
 }
